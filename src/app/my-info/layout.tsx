6197a3002285c5a2566a54704a1a5a0ff69a6d04
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-09-15 18:54:04 +0200

    feat: add aside

--- src/app/my-info/layout.tsx
+++ src/app/my-info/layout.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Aside from "@/components/Aside/Aside";
 import Dashboard from "@/components/Dashboard/Dashboard";
 import Header from "@/components/Header/Header";
 
@@ -6,10 +7,13 @@ export default function MainPageLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <>
+    <div className="bg-slate-100">
       <Header />
       <Dashboard />
-      {children}
-    </>
+      <div className="flex">
+        <Aside />
+        {children}
+      </div>
+    </div>
   );
 }
---
+++ src/components/Aside/Aside.tsx
@@ -0,0 +1,75 @@
+import {
+  CircleUser,
+  Clock,
+  FacebookIcon,
+  Globe,
+  Hash,
+  LinkedinIcon,
+  Mail,
+  MapPin,
+  Phone,
+  Twitter,
+  Users,
+} from "lucide-react";
+import { Card } from "../ui/card";
+
+export default function Aside() {
+  return (
+    <aside className="relative max-w-[225px] mx-6">
+      <div className="relative -top-[14px] flex flex-col gap-4">
+        <Card className="bg-white text-sm p-6">
+          <a href="tel:07911654321" className="flex gap-2">
+            <Phone className="size-4" /> 07911 654321
+          </a>
+          <a href="mailto:avd.yana@videorollnet" className="flex my-2 gap-2">
+            <Mail className="size-4" />
+            avd.yana@videorollnet
+          </a>
+          <div className="flex gap-[14px]">
+            <LinkedinIcon className="size-4" />
+            <FacebookIcon className="size-4" />
+            <Twitter className="size-4" />
+          </div>
+        </Card>
+        <Card className="bg-white text-sm p-6">
+          <div className="mb-4">Hire Date</div>
+          <div>
+            <div className="mb-2">Sep. 3,2020</div>
+            <div>3y - 9m - 20d</div>
+          </div>
+        </Card>
+        <Card className="bg-white text-sm p-6 flex flex-col gap-2">
+          <div className="flex gap-2">
+            <Hash className="size-4" /> 5
+          </div>
+          <div className="flex gap-2">
+            <Clock className="size-4" /> Full-Time
+          </div>
+          <div className="flex gap-2">
+            <Users className="size-4" /> Operations
+          </div>
+          <div className="flex gap-2">
+            <Globe className="size-4" /> Europe
+          </div>
+          <div className="flex gap-2">
+            <MapPin className="size-4" /> London, UK
+          </div>
+        </Card>
+        <Card className="bg-white text-sm p-6">
+          <div className="mb-4">Direct Reports</div>
+          <div className="flex flex-col gap-2">
+            {["Shane", "Nathan", "Mitchell", "Philip"].map((user) => (
+              <div key={user} className="flex gap-2">
+                <CircleUser className="size-4" />
+                {user}
+              </div>
+            ))}
+            <div className="flex gap-2">
+              <Users className="size-4" /> 4 more ...
+            </div>
+          </div>
+        </Card>
+      </div>
+    </aside>
+  );
+}
