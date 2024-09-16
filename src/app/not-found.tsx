c9e64c5f5e8edb66e76f3464ed58eb19a0b30c73
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-09-16 11:01:10 +0200

    feat: add not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
+
 export default function NotFound() {
-  return <div>not-found</div>;
+  return (
+    <div className="w-full h-full text-center">
+      <h1 className="text-3xl font-bold my-[10%]">
+        The page you are looking for was not found
+      </h1>
+      <Link href="/" className="p-4 text-xl rounded-lg border">
+        Go Home
+      </Link>
+    </div>
+  );
 }
