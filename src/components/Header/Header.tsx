d052e062ce3b1874fcf750227750f0ea53a5dda2
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-09-16 13:41:26 +0200

    featL hide Header in login page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -46,39 +46,42 @@ export default function Header() {
   const firstLevelPage = pathname.split("/")[1];
 
   return (
-    <header className="w-full flex justify-between items-start pt-8 px-6">
-      <h1 className="font-semibold text-xl">HarmonyHR</h1>
-      <NavigationMenu className="self-end">
-        <NavigationMenuList>
-          {PAGES.map((page) => (
-            <NavigationMenuItem key={page.path}>
-              <Link
-                className={`${
-                  firstLevelPage === page.path ? "bg-slate-200" : ""
-                } inline-block text-lg p-4 rounded-t-xl`}
-                href={`/${page.path}`}
-              >
-                {page.name}
-              </Link>
-            </NavigationMenuItem>
-          ))}
-        </NavigationMenuList>
-      </NavigationMenu>
-      <Input
-        className="max-w-[395px] text-lg border border-black"
-        placeholder="Search"
-      />
-      <div className="flex gap-6 justify-center items-center">
-        <Settings className="size-6" />
-        <QuestionMarkCircledIcon className="size-6" />
-        <Bell className="size-6" />
-        <Avatar className={`${avatar ? "" : "animate-pulse"} size-[38px]`}>
-          <AvatarImage src={avatar || ""} alt="user photo" />
-          <AvatarFallback>
-            {name ? name!.charAt(0).toUpperCase() : ""}
-          </AvatarFallback>
-        </Avatar>
-      </div>
-    </header>
+    firstLevelPage &&
+    firstLevelPage !== "login" && (
+      <header className="w-full flex justify-between items-start pt-8 px-6">
+        <h1 className="font-semibold text-xl">HarmonyHR</h1>
+        <NavigationMenu className="self-end">
+          <NavigationMenuList>
+            {PAGES.map((page) => (
+              <NavigationMenuItem key={page.path}>
+                <Link
+                  className={`${
+                    firstLevelPage === page.path ? "bg-slate-200" : ""
+                  } inline-block text-lg p-4 rounded-t-xl`}
+                  href={`/${page.path}`}
+                >
+                  {page.name}
+                </Link>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+        <Input
+          className="max-w-[395px] text-lg border border-black"
+          placeholder="Search"
+        />
+        <div className="flex gap-6 justify-center items-center">
+          <Settings className="size-6" />
+          <QuestionMarkCircledIcon className="size-6" />
+          <Bell className="size-6" />
+          <Avatar className={`${avatar ? "" : "animate-pulse"} size-[38px]`}>
+            <AvatarImage src={avatar || ""} alt="user photo" />
+            <AvatarFallback>
+              {name ? name!.charAt(0).toUpperCase() : ""}
+            </AvatarFallback>
+          </Avatar>
+        </div>
+      </header>
+    )
   );
 }
